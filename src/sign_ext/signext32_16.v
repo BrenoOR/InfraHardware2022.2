8509cb9a17b0adb3014827a240fcd08fbc8731dd
Breno <bor@cin.ufpe.br>
2023-02-17 11:09:47 -0300

    First implementation of sign extend 16 -> 32

--- src/sign_ext/signext32_16.v
+++ src/sign_ext/signext32_16.v
@@ -1,6 +1,8 @@
 module signext32_16 (
-    input wire [15:0] data_input;
-    output wire [31:0] data_out;
+    input wire [15:0] data_input,
+    output wire [31:0] data_out
 );
+
+    assign data_out = {{16{data_input[15]}}, data_input};
 
 endmodule
