401d021d1daa2b48773119ef9d83ee899431a5a9
Pedro Barros de Souza Lima <101725415+pbsl-cin@users.noreply.github.com>
2023-03-11 15:41:13 -0300

    Update mult.v

--- src/mult/mult.v
+++ src/mult/mult.v
@@ -4,44 +4,10 @@ module complement2(
     output wire [31:0] complemento 
 );
 
-    wire [31:0] temp;
-
-    always@(*) begin
-
-        temp[0] = ~orgnValue[0];
-        temp[1] = ~orgnValue[1];
-        temp[2] = ~orgnValue[2];
-        temp[3] = ~orgnValue[3];
-        temp[4] = ~orgnValue[4];
-        temp[5] = ~orgnValue[5];
-        temp[6] = ~orgnValue[6];
-        temp[7] = ~orgnValue[7];
-        temp[8] = ~orgnValue[8];
-        temp[9] = ~orgnValue[9];
-        temp[10] = ~orgnValue[10];
-        temp[11] = ~orgnValue[11];
-        temp[12] = ~orgnValue[12];
-        temp[13] = ~orgnValue[13];
-        temp[14] = ~orgnValue[14];
-        temp[15] = ~orgnValue[15];
-        temp[16] = ~orgnValue[16];
-        temp[17] = ~orgnValue[17];
-        temp[18] = ~orgnValue[18];
-        temp[19] = ~orgnValue[19];
-        temp[20] = ~orgnValue[20];
-        temp[21] = ~orgnValue[21];
-        temp[22] = ~orgnValue[22];
-        temp[23] = ~orgnValue[23];
-        temp[24] = ~orgnValue[24];
-        temp[25] = ~orgnValue[25];
-        temp[26] = ~orgnValue[26];
-        temp[27] = ~orgnValue[27];
-        temp[28] = ~orgnValue[28];
-        temp[29] = ~orgnValue[29];
-        temp[30] = ~orgnValue[30];
-        temp[31] = ~orgnValue[31];
-
-        temp = temp + 32'b00000000000000000000000000000001
+    reg [31:0] temp;
+    
+    always @(*)begin
+        temp = (~orgnValue + 32'b00000000000000000000000000000001);
     end
 
     assign complemento = temp;
@@ -51,30 +17,34 @@ endmodule
 module booth_action(
     input wire [64:0] A,
     input wire [64:0] S,
-    input wire [64:0] P,
+    input wire [64:0] this_P,
     output wire [64:0] new_P
 );
 
-    assign wire Qn = P[1];
-    assign wire Qn1 = P[0];
+    wire Qn = this_P[1];
+    wire Qn1 = this_P[0];
+    reg [64:0] temp;
+    integer teste;
 
     always @(*)begin
         if(Qn != Qn1)begin
             if(Qn == 1'b0)begin
-                new_P = P + A;
+                teste = 1;
+                temp = this_P + A;
             end
             else begin
-                new_P = P + S;
+                teste = 2;
+                temp = this_P + S;
             end
         end
         else begin
-            new_P = P;
+            teste = 3;
+            temp = this_P;
         end
-        case(new_P[64])
-            1'b1: new_P = {1'b1, P[63:0]};
-            default: new_P = {1'b0, P[63:0]}
-        endcase
+        temp = {temp[64], temp[64:1]};
     end
+
+    assign new_P = temp;
 
 endmodule
 
@@ -89,51 +59,50 @@ module booth_mult(
 );
 
     wire [31:0] nBR;
-    //wire [31:0] acc = 32'b00000000000000000000000000000000;
-    //wire [31:0] leastSig = valueB;
-    //wire Qn = 1'b0;
+    //32'b00000000000000000000000000000011;
+    //32'b00000000000000000000000000001100;
 
     complement2 comp(clock, valueA, nBR);
 
-    assign wire [64:0] A = {valueA, 33'b000000000000000000000000000000000};
-    assign wire [64:0] S = {nBR, 33'b000000000000000000000000000000000};
-    assign wire [64:0] P = {33'b000000000000000000000000000000000, valueB};
+    wire [64:0] A = {valueA, 33'b000000000000000000000000000000000};
+    wire [64:0] S = {nBR, 33'b000000000000000000000000000000000};
+    wire [64:0] P[32:0];
+    assign P[0] = {32'b00000000000000000000000000000000, valueB, 1'b0};
+    
+    booth_action a1(A, S, P[0], P[1]);
+    booth_action a2(A, S, P[1], P[2]);
+    booth_action a3(A, S, P[2], P[3]);
+    booth_action a4(A, S, P[3], P[4]);
+    booth_action a5(A, S, P[4], P[5]);
+    booth_action a6(A, S, P[5], P[6]);
+    booth_action a7(A, S, P[6], P[7]);
+    booth_action a8(A, S, P[7], P[8]);
+    booth_action a9(A, S, P[8], P[9]);
+    booth_action a10(A, S, P[9], P[10]);
+    booth_action a11(A, S, P[10], P[11]);
+    booth_action a12(A, S, P[11], P[12]);
+    booth_action a13(A, S, P[12], P[13]);
+    booth_action a14(A, S, P[13], P[14]);
+    booth_action a15(A, S, P[14], P[15]);
+    booth_action a16(A, S, P[15], P[16]);
+    booth_action a17(A, S, P[16], P[17]);
+    booth_action a18(A, S, P[17], P[18]);
+    booth_action a19(A, S, P[18], P[19]);
+    booth_action a20(A, S, P[19], P[20]);
+    booth_action a21(A, S, P[20], P[21]);
+    booth_action a22(A, S, P[21], P[22]);
+    booth_action a23(A, S, P[22], P[23]);
+    booth_action a24(A, S, P[23], P[24]);
+    booth_action a25(A, S, P[24], P[25]);
+    booth_action a26(A, S, P[25], P[26]);
+    booth_action a27(A, S, P[26], P[27]);
+    booth_action a28(A, S, P[27], P[28]);
+    booth_action a29(A, S, P[28], P[29]);
+    booth_action a30(A, S, P[29], P[30]);
+    booth_action a31(A, S, P[30], P[31]);
+    booth_action a32(A, S, P[31], P[32]);
 
-    //booth_action a1(valueA, valueB, nBR, Qn, acc, leastSig);
-    booth_action a1(A, S, P, P);
-    booth_action a2(A, S, P, P);
-    booth_action a3(A, S, P, P);
-    booth_action a4(A, S, P, P);
-    booth_action a5(A, S, P, P);
-    booth_action a6(A, S, P, P);
-    booth_action a7(A, S, P, P);
-    booth_action a8(A, S, P, P);
-    booth_action a9(A, S, P, P);
-    booth_action a10(A, S, P, P);
-    booth_action a11(A, S, P, P);
-    booth_action a12(A, S, P, P);
-    booth_action a13(A, S, P, P);
-    booth_action a14(A, S, P, P);
-    booth_action a15(A, S, P, P);
-    booth_action a16(A, S, P, P);
-    booth_action a17(A, S, P, P);
-    booth_action a18(A, S, P, P);
-    booth_action a19(A, S, P, P);
-    booth_action a20(A, S, P, P);
-    booth_action a21(A, S, P, P);
-    booth_action a22(A, S, P, P);
-    booth_action a23(A, S, P, P);
-    booth_action a24(A, S, P, P);
-    booth_action a25(A, S, P, P);
-    booth_action a26(A, S, P, P);
-    booth_action a27(A, S, P, P);
-    booth_action a28(A, S, P, P);
-    booth_action a29(A, S, P, P);
-    booth_action a30(A, S, P, P);
-    booth_action a31(A, S, P, P);
-    booth_action a32(A, S, P, P);
-
-    assign wire mostSig = P[64:33];
-    assign wire leastSig = P[32:1];
+    assign mostSig = P[32][64:33];
+    assign leastSig = P[32][32:1];
 
 endmodule
